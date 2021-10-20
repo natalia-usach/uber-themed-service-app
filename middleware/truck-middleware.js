3d8999bd3301aee6c97db68ba423b2346fbe2287
Natalia Usach <klimchuknatalia@gmail.com>
2021-10-20 08:47:42 +0000

    Upload New File

---
+++ middleware/truck-middleware.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+
+module.exports = async function(req, res, next) {
+    if (req.method === 'OPTIONS') {
+      next();
+    }
+  
+    try {
+        const user = await User.findOne({email: req.user.email});
+        if(user.role === 'DRIVER') {
+            next();
+        } else {
+            return res.status(400).send({message: 'Available only for drivers'});
+        }
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({message: 'Client error'});
+    }
+  };
