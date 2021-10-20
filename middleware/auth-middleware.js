76d1d25019911994d62017e596b67cd061da1854
Natalia Usach <klimchuknatalia@gmail.com>
2021-10-20 08:35:42 +0000

    Upload New File

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const {secret} = require('../config');
+
+module.exports = function(req, res, next) {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return res.status(401).send({message: 'Unauthorized user'});
+    }
+
+    const decodedData = jwt.verify(token, secret);
+    req.user = decodedData;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({message: error.message});
+  }
+};
